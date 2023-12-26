1dc5ca04e3f997e0ca20ca6b7069c7fdcfc231ae
jgmalinov <gogotk24@gmail.com>
2023-12-26 20:36:13 +0200

    residual changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using JobBoardApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- obj/Debug/net8.0/JobBoardApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/JobBoardApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobBoardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3214972efdcc3d2b70832afc673003bffb7067b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobBoardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobBoardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
